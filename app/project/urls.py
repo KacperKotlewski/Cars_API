851f9a46c583d41f7dfea1e1fe39803f1b02e3c3
KacperKotlewski <kotlewski.kacper@gmail.com>
2020-08-28 16:33:36 +0200

    updated urls, added static dirs and docs views

--- app/project/urls.py
+++ app/project/urls.py
@@ -1,20 +1,43 @@
-# -*- coding: utf-8 -*-
-"""project URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path, re_path
+from django.views.static import serve
+from rest_framework import permissions
 
-urlpatterns = [path("admin/", admin.site.urls)]
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+from .settings import MEDIA_ROOT, STATIC_ROOT
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Cars API", default_version="v1", description="API to fetch and rate cars"
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+docs_urlpatterns = [
+    path(
+        "docs/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger"
+    ),
+    path(
+        "docs-redoc/",
+        schema_view.with_ui("redoc", cache_timeout=0),
+        name="schema-redoc",
+    ),
+]
+
+statics_urlpatterns = [
+    url(r"^media/(?P<path>.*)$", serve, {"document_root": MEDIA_ROOT}),
+    url(r"^static/(?P<path>.*)$", serve, {"document_root": STATIC_ROOT}),
+]
+
+urlpatterns = (
+    [
+        path("admin/", admin.site.urls),
+        re_path(r"^api-auth/", include("rest_framework.urls")),
+    ]
+    + docs_urlpatterns
+    + statics_urlpatterns
+)
