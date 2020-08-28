164b4f96d083c6f5519cb4e4a7f489aa33f91929
KacperKotlewski <kotlewski.kacper@gmail.com>
2020-08-28 16:49:49 +0200

    created Car and CarRate models and made migrations

--- app/api/models.py
+++ app/api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    make = models.CharField(max_length=128)
+    model = models.CharField(max_length=128)
+
+    def __str__(self):
+        return "car(" + str(self.pk) + ") make:" + self.make + " model:" + self.model
+
+    class Meta:
+        unique_together = ("make", "model")
+
+
+class CarRate(models.Model):
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    rate = models.IntegerField()
+
+    def __str__(self):
+        return (
+            "rate(" + str(self.pk) + f") got rate of {self.rate} for " + str(self.car)
+        )
